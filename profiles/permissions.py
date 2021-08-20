1e5fd383f390d3440360c57472e09233c26b876c
Tommasory <tommasory@gmail.com>
2021-08-20 16:46:20 -0500

    Editando Permisos del API.

--- profiles/permissions.py
+++ profiles/permissions.py
@@ -9,3 +9,13 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """ Permite actualizar propio Status Feed """
+
+    def has_object_permission(self, request, view, obj):
+        """ Chequear si un usuario esta editanto su propio perfil """
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile_id == request.user.id
--- profiles/views.py
+++ profiles/views.py
@@ -6,6 +6,7 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated, IsAuthenticated
 
 from . import serializers, models, permissions
 
@@ -124,6 +125,10 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticated,
+    )
 
     def perform_create(self, serializer):
         """ Setear el perfil de usuario para el usuario que esta logeado """
